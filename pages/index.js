a2a0a58d8a498cb85289c81cf2336344847e15d3
Ariann Farias <ariannmichael@gmail.com>
2021-01-30 19:23:41 -0300

    Aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
@@ -7,17 +7,9 @@ import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
@@ -37,8 +29,13 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <form onSubmit={handleSubmit}>
-              <input placeholder="Insira seu nome" onChange={(e) => setName(e.target.value)} />
-              <button type="submit" disabled={name.length === 0}>Jogar</button>
+              <Input
+                name="nomeDoUsuario"
+                placeholder="Insira seu nome"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>Jogar</Button>
             </form>
           </Widget.Content>
         </Widget>
